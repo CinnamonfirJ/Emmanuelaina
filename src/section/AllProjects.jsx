10dc98de54d2171bb8d2899f57562b2ebe8087b6
CinnamonfirJ <cinnamonfir@gmail.com>
2024-12-31 21:06:46 +0100

    modified:   src/section/AllProjects.jsx

--- src/section/AllProjects.jsx
+++ src/section/AllProjects.jsx
@@ -1,16 +1,21 @@
 import arrow from "../assets/arrow.png";
 import myProject from "../assets/grommet-icons_projects.png";
 import SectionHead from "../components/SectionHead";
-import project1 from "../assets/project1.png";
-import project2 from "../assets/project2.png";
-import project3 from "../assets/project3.png";
-import project4 from "../assets/project4.png";
-import project5 from "../assets/project5.png";
-import project6 from "../assets/project6.png";
+import project1 from "../assets/projects/project1.png";
+import project2 from "../assets/projects/project2.png";
+import project3 from "../assets/projects/project3.png";
+import project4 from "../assets/projects/project4.png";
+import project5 from "../assets/projects/project5.png";
+import project6 from "../assets/projects/project6.png";
+import project7 from "../assets/projects/project7.png";
+import project8 from "../assets/projects/project8.png";
+import project9 from "../assets/projects/project9.png";
+
 import Button from "../components/Button";
 import "aos/dist/aos.css";
 import AOS from "aos";
 import { useEffect } from "react";
+import { Link2 } from "lucide-react";
 
 const AllProject = () => {
   useEffect(() => {
@@ -20,39 +25,57 @@ const AllProject = () => {
   const projects = [
     {
       imgUrl: project1,
-      title: "Fifty Crypto Settings",
-      category: "Fintech-Paid Project",
-      link: "/",
+      title: "ChatNaija - AI Chatbot",
+      category: "AI-Team Project",
+      link: "https://chat-naija-ai.vercel.app/",
     },
     {
       imgUrl: project2,
-      title: "Admin Management Dashboard",
+      title: "Fifty Crypto Exchange (App)",
       category: "Fintech-Paid Project",
-      link: "/",
+      link: "https://www.behance.net/gallery/211947745/Fifty-Xchange-App",
     },
     {
       imgUrl: project3,
-      title: "Crypto App Sign Up",
-      category: "Fintech-Paid Project",
-      link: "/",
+      title: "Fifty Crypto Exchange (Web)",
+      category: "Web3 Fintech-Paid Project",
+      link: "https://www.behance.net/gallery/214671509/Fifty-Xchange",
     },
     {
       imgUrl: project4,
       title: "Burna Coin (Tap to Earn)",
-      category: "Web 3 Memecoin-Paid Project",
-      link: "/",
+      category: "Web3 Memecoin-Personal Project",
+      link: "https://www.behance.net/gallery/214716603/Burna-Coin",
     },
     {
       imgUrl: project5,
       title: "Shopswap",
-      category: "E Commerce- Personal Project",
-      link: "/",
+      category: "E Commerce-Personal Project",
+      link: "https://www.behance.net/gallery/201544379/ShopSwap-E-Commerce-App",
     },
     {
       imgUrl: project6,
       title: "Metamint NFT Dashboard",
+      category: "Web3 Fintech-Personal Project",
+      link: "https://www.behance.net/gallery/202765619/NFT-Dashboard-Design",
+    },
+    {
+      imgUrl: project7,
+      title: "EventPulse",
+      category: "Social-Personal Project",
+      link: "https://www.behance.net/gallery/210547441/Event-Management-Sign-Up-Page",
+    },
+    {
+      imgUrl: project8,
+      title: "Admin Management Dashboard",
+      category: "Fintech-Paid Project",
+      link: "https://www.behance.net/gallery/206867637/Transactions-Management-Dashboard",
+    },
+    {
+      imgUrl: project9,
+      title: "Web3 Finance OS Dashboard",
       category: "Web3 Fintech-Paid Project",
-      link: "/",
+      link: "https://www.behance.net/gallery/200587595/Web3-Financial-OS-Bank-Settings-Section",
     },
   ];
 
@@ -88,9 +111,12 @@ const AllProject = () => {
                   <div>
                     <a
                       href={project.link}
+                      target='_blank'
                       className='flex justify-center items-center gap-2 bg-primary rounded-full py-1 px-2 text-gray-200'
                     >
-                      <div>View</div>
+                      <div>
+                        <Link2 size={20} />
+                      </div>
                       <div>
                         <img src={arrow} alt='arrow' />
                       </div>
